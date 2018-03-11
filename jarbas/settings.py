7fdf527c4bc4839ec30509bd144477479021dcc7
Irio Musskopf <irio@protonmail.com>
2018-03-11 12:20:27 -0300

    Define an easy to read value as default Django secret key

--- jarbas/settings.py
+++ jarbas/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import secrets
 
 from decouple import Csv, config
 from dj_database_url import parse
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default=secrets.token_hex())
+SECRET_KEY = config('SECRET_KEY', default='my-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
